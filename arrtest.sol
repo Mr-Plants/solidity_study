b5b988bdaae2087580487521a3a71e62d1a1543f
Remix Workspace <>
2022-10-31 18:12:56 +0800

    arr

--- arrtest.sol
+++ arrtest.sol
@@ -1,10 +1,18 @@
-pragma solidity 0.4.0;
+pragma solidity ^0.4.0;
 
 contract Arr {
     uint[] public a = [1, 2, 3];
+    string public s = 'afsdjb';
 
-    function h() public view returns (int){
+    function h() public view returns (uint){
         return a.length;
     }
 
+    function v() public view returns (uint){
+        return bytes(s).length;
+    }
+
+    function v2() public view returns (byte){
+        return bytes(s)[1];
+    }
 }
