88f7733a959d1ec80b2f28ae2c34ed548026df80
Remix Workspace <>
2022-11-02 15:41:46 +0800

    coding...

--- simple_token.sol
+++ simple_token.sol
@@ -3,26 +3,41 @@ pragma solidity ^0.4.20;
 contract ERC20Interface {
     string name;
     string symbol;
-    uint decimals;
-    uint totalSupply;
+    uint8 decimals;
+    uint256 totalSupply;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success);
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success);
 
-    function approve(address _spender, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success);
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining);
 }
 
 contract MyToken is ERC20Interface {
-    mapping(address => uint) balanceOf;
-    mapping(address => mapping(address => uint)) internal allowed;
+    mapping(address => uint256)public balanceOf;
+    mapping(address => mapping(address => uint256)) internal allowed;
 
-    constructor(){
+    constructor() public {
         name = "wei";
         symbol = "brutesWei";
         decimals = 0;
@@ -31,7 +46,10 @@ contract MyToken is ERC20Interface {
         // 铸币人拥有全部
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success){
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
         require(_to != address(0), "转账地址为空");
         require(balanceOf[msg.sender] >= _value, "余额不足");
         require(balanceOf[_to] + _value >= balanceOf[_to], "余额溢出");
@@ -42,7 +60,11 @@ contract MyToken is ERC20Interface {
         emit Transfer(msg.sender, _to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
         require(_to != address(0), "转账地址为空");
         require(balanceOf[_from] >= _value, "余额不足");
         require(balanceOf[_to] + _value >= balanceOf[_to], "余额溢出");
@@ -53,13 +75,20 @@ contract MyToken is ERC20Interface {
         emit Transfer(_from, _to, _value);
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success){
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
         allowed[msg.sender][_spender] = _value;
         success = true;
         emit Approval(msg.sender, _spender, _value);
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 remaining)
+    {
         remaining = allowed[_owner][_spender];
     }
 }
